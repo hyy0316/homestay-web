c0d7924d4a7ed8b51ac22eb788c361706450605c
yyhuang <yyhuang@amarsoft.com>
2020-04-13 01:55:56 +0800

    （首页）：添加客户页面

--- homestay/api/adminApi.js
+++ homestay/api/adminApi.js
@@ -2,10 +2,10 @@
 const AdminListUrl = baseUrl + "api/admin/findAdminList";
 //
 // //根据id 查询管理员信息    接口 ??
-// const findAdminList = baseUrl + "amdin/getAdminById";
+// const findAdminList = baseUrl + "admin/getAdminById";
+//
+//添加管理员列表信息接口
+const insertAdminUrl = baseUrl + "api/admin/insertAdmin";
 //
 // //查询管理员列表信息接口
-// const findAdminList = baseUrl + "amdin/findAdminList";
-//
-// //查询管理员列表信息接口
-// const findAdminList = baseUrl + "amdin/findAdminList";
+// const findAdminList = baseUrl + "admin/findAdminList";
