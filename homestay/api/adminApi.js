0f1faf70b4e43bf0dfcfbcdb305ef618aaaac0f5
yyhuang <yyhuang@amarsoft.com>
2020-04-14 21:06:29 +0800

    （管理员）：完成管理员修改

--- homestay/api/adminApi.js
+++ homestay/api/adminApi.js
@@ -1,11 +1,15 @@
 //查询管理员列表信息接口
 const AdminListUrl = baseUrl + "api/admin/findAdminList";
-//
-// //根据id 查询管理员信息    接口 ??
-// const findAdminList = baseUrl + "admin/getAdminById";
+
+//根据id 查询管理员信息    接口 ??
+const getAdminById = baseUrl + "api/admin/getAdminById";
+
 //
 //添加管理员列表信息接口
 const insertAdminUrl = baseUrl + "api/admin/insertAdmin";
-//
-// //查询管理员列表信息接口
-// const findAdminList = baseUrl + "admin/findAdminList";
+
+//删除管理员列表信息接口
+const deletebyAdminUrl = baseUrl + "api/admin/deletebyAdmin";
+
+//修改管理员列表信息接口
+const upadataAdminrl = baseUrl + "api/admin/upadataAdmin";
