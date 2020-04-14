955e011a8a9f850033d9392afb3de1f7a7f5df4f
yyhuang <yyhuang@amarsoft.com>
2020-04-15 04:36:39 +0800

    feat（客户）：完成添加客户

---
+++ homestay/api/guestApi.js
@@ -0,0 +1,15 @@
+//查询客户列表信息接口
+const GuestListUrl = baseUrl + "api/guest/findGuestList";
+
+//根据id 查询客户信息    接口 ??
+const getGuestById = baseUrl + "api/guest/getGuestById";
+
+//
+//添加客户列表信息接口
+const insertGuestUrl = baseUrl + "api/guest/insertGuest";
+
+//删除客户列表信息接口
+const deletebyGuestUrl = baseUrl + "api/guest/deletebyGuest";
+
+//修改客户列表信息接口
+const upadataGuestrl = baseUrl + "api/guest/upadataGuest";
